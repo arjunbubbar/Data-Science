858b9bb31f45f47dfe88d5a9f919524e45147811
arjunbubbar <arjunbubbar@gmail.com>
2025-03-03 15:27:19 +0000

    Add files via upload

---
+++ Series.py
@@ -0,0 +1,39 @@
+# pd for pandas
+
+import pandas as pd
+
+nums = [1,3,4,34,321,925,321,45,4,26,1,29,4,87]
+
+print (nums)
+print (type (nums))
+
+ser = pd.Series (nums)
+
+print (ser)
+print (type (ser))
+
+# Series prints the index and the values
+
+# Prints the index
+
+print (ser [3])
+
+# Statistical functions
+
+print (ser.sum ())
+print (ser.min ())
+print (ser.max ())
+print (ser.mean ())
+print (ser.count ())
+print (ser.median ())
+print (ser.mode ())
+
+# Count of unique values, categorises them
+
+print (ser.value_counts ())
+
+# Sorting in either increasing or decreasing order
+
+print (ser.sort_values ())
+
+
